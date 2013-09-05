ca4965c45b68dfbfc7788a5642f345e2e81b984b
Ratser Wang <morsel.wang@gmail.com>
2013-09-05 11:46:36 +0800

    exercise 4.21

---
+++ ch4/exercise4.21.rkt
@@ -0,0 +1,9 @@
+;;; exercise 4.21
+
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) #t (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) #f (ev? ev? od? (- n 1))))))
