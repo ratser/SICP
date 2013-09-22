1fd55a703dcb98ad520b87fa4892d1ff470b81fc
Ratser Wang <morsel.wang@gmail.com>
2013-09-22 16:22:30 +0800

    delete some files

--- ch4/exercise4.2.rkt
+++
@@ -1,9 +0,0 @@
-;;; exercise4.2
-
-;;;   the implementation of application? operator and
-;;; operands should be changed respectively.
-
-(define (application? exp)
-  (eq? (get-tag exp) 'call))
-(define (operator exp) (cadr exp))
-(define (operands exp) (cddr exp))
