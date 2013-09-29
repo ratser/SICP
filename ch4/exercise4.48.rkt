ef37777a3af92586097862fb5d115a55c5b53964
Ratser Wang <morsel.wang@gmail.com>
2013-09-29 14:33:15 +0800

    rename

--- ch4/exercise4.48.rkt
+++ ch4/exercise4.48.rkt
@@ -4,16 +4,18 @@
   (cond ((null? lst) false)
         ((eq? item (car lst)) true)
         (else (memq item (cdr lst)))))
+
 (define nouns '(noun student professor cat class))
 (define verbs '(verb studies lectures eats sleeps))
 (define articles '(article the a))
+(define prepositions '(prep for to in by with))
 
 (define (parse-sentence)
   (list 'sentence
         (parse-noun-phrase)
-        (parse-word verbs)))
-(define (parse-noun-phrase)
-  (list 'noun-phrase
+        (parse-verb-phrase)))
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
         (parse-word articles)
         (parse-word nouns)))
 (define (parse-word word-list)
@@ -22,6 +24,25 @@
   (let ((found-word (car *unparsed*)))
     (set! *unparsed* (cdr *unparsed*))
     (list (car word-list) found-word)))
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+         (maybe-extend (list 'verb-phrase
+                             verb-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+         (maybe-extend (list 'noun-phrase
+                             noun-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
 
 (define *unparsed* '())
 (define (parse input)
